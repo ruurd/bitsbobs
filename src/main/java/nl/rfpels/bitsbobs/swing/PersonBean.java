f4bed8c317b51ffe1fb4b51db0480b29daee3e2d
Ruurd Pels <rfpels@gmail.com>
2019-06-21 17:33:24 +0200

    xyzzy

---
+++ src/main/java/nl/rfpels/bitsbobs/swing/PersonBean.java
@@ -0,0 +1,15 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2018 Bureau Pels. All Rights Reserved.
+//------------------------------------------------------------------------------
+package nl.rfpels.bitsbobs.swing;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PersonBean {
+    private String voornaam;
+    private String achternaam;
+    private String leeftijd;
+}
